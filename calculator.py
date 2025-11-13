1f4ec609616de0024cdee16bbfb2dbcabe16e279
huweewee <20180013824@my.xu.edu.ph>
2025-11-13 21:27:42 +0800

    Implement basic calculator structure

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
+# Hello World
+import subprocess
+
+def Calc():
+    print("please enter an option")
+    print("1 for addition")
+    print("2 for multiply")
+    print("3 for Subtraction")
+    print("4 for Division")
+    choice = int(input("Please enter Choice Here"))
+
+    if choice == 1:
+        subprocess.run(["python", "Addition.py"]) 
+    elif choice == 2:
+        subprocess.run(["python", "Multiply.py"]) 
+    elif choice == 3:
+        subprocess.run(["python", "Subtract.py"]) 
+    elif choice == 4:
+        subprocess.run(["python", "Divide.py"])         
+    else:
+        print("no script executed")
+
+Calc()
 
